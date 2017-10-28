02c00cd053db3d914ecddbc2117c945e4ec907c5
Jacob Garby <j.garby@icloud.com>
2017-10-28 14:59:40 +0100

    using tgui library

---
+++ src/scenes/Game.hpp
@@ -0,0 +1,10 @@
+#ifndef SCENES_GAME_HPP
+#define SCENES_GAME_HPP
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+#include "../include/Scene.hpp"
+#include "../include/assetregisters.hpp"
+
+#endif
